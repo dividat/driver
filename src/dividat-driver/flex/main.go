d8caf08ea086b79e593b66d2f24cb3a324738938
Ignas Vyšniauskas <ignas@dividat.ch>
2025-11-10 14:32:37 +0200

    Move serial chunking outside and add Sensitronics support

--- src/dividat-driver/flex/main.go
+++ src/dividat-driver/flex/main.go
@@ -14,10 +14,7 @@ The functionality of this module is as follows:
 */
 
 import (
-	"bufio"
-	"bytes"
 	"context"
-	"encoding/binary"
 	"net/http"
 	"strconv"
 	"strings"
@@ -29,6 +26,8 @@ import (
 	"go.bug.st/serial"
 	"go.bug.st/serial/enumerator"
 
+	"github.com/dividat/driver/src/dividat-driver/flex/sensingtex"
+	"github.com/dividat/driver/src/dividat-driver/flex/sensitronics"
 	"github.com/dividat/driver/src/dividat-driver/util"
 	"github.com/dividat/driver/src/dividat-driver/util/websocket"
 )
@@ -105,6 +104,21 @@ func (backend *DeviceBackend) broadcastStatusUpdate() {
 	backend.broadcastMessage(websocket.Message{Status: &status})
 }
 
+type SerialReader interface {
+	// Read from the serial port and pipe its signal into the callback, summarizing
+	// package units into a buffer. Forward commands from client.
+	ReadFromSerial(ctx context.Context, cancel context.CancelFunc, logger *logrus.Entry, port serial.Port, tx chan interface{}, onReceive func([]byte))
+}
+
+func deviceToReader(deviceInfo websocket.UsbDeviceInfo) SerialReader {
+	if deviceInfo.Manufacturer == "Teensyduino" {
+		return &sensingtex.SensingTexReader{}
+	} else if deviceInfo.Manufacturer == "Sensitronics" {
+		return &sensitronics.SensitronicsReader{}
+	}
+	return nil
+}
+
 // connect to a "validated" device
 func (backend *DeviceBackend) connectInternal(device websocket.UsbDeviceInfo) error {
 	// Only allow one connection change at a time
@@ -128,6 +142,14 @@ func (backend *DeviceBackend) connectInternal(device websocket.UsbDeviceInfo) er
 		backend.log.WithField("path", device.Path).WithField("error", err).Info("Failed to open connection to serial port.")
 		return err
 	}
+	reader := deviceToReader(device)
+	// should not happen
+	if reader == nil {
+		backend.log.WithField("device", device).Error("Could not find reader for device!")
+		port.Close()
+		return err
+	}
+
 	backend.currentDevice = &device
 
 	backend.cancelCurrentConnection = func() {
@@ -139,9 +161,11 @@ func (backend *DeviceBackend) connectInternal(device websocket.UsbDeviceInfo) er
 	}
 	backend.broadcastStatusUpdate()
 
+	tx := backend.broker.Sub(brokerTopicTx)
+
 	// TODO: seems to work, but look at ctx/cancel a bit more carefully, at
 	// a minimum it feels like some parts are redundant
-	go connectSerial(ctx, backend.cancelCurrentConnection, backend.log, port, backend.broker.Sub(brokerTopicTx), onReceive)
+	go reader.ReadFromSerial(ctx, backend.cancelCurrentConnection, backend.log, port, tx, onReceive)
 	return nil
 }
 
@@ -353,53 +377,6 @@ func (backend *DeviceBackend) ProcessFirmwareUpdateRequest(command websocket.Upd
 	return
 }
 
-// Serial communication
-
-type ReaderState int
-
-const (
-	WAITING_FOR_HEADER ReaderState = iota
-	HEADER_START
-	HEADER_READ_LENGTH_MSB
-	HEADER_READ_LENGTH_LSB
-	WAITING_FOR_BODY
-	BODY_START
-	BODY_READ_SAMPLE
-	UNEXPECTED_BYTE
-)
-
-const (
-	HEADER_START_MARKER = 'N'
-	BODY_START_MARKER   = 'P'
-)
-
-const (
-	// row, column and pressure value, one uint8 each
-	BYTES_PER_SAMPLE_8BIT = 3
-
-	// same as above, but pressure value is 2 bytes (uint16), big-endian
-	// Note: Sensing Tex docs state value max is 2^12-1 (hence "12bit"),
-	// but in practice they seem to send values up to ~9000, so more like
-	// "14 bit".
-	BYTES_PER_SAMPLE_12BIT = 4
-)
-
-var BITDEPTH_8_CMD = []byte{'U', 'L', '\n'}
-
-var BITDEPTH_12_CMD = []byte{'U', 'M', '\n'}
-
-func isBitdepthCommand(cmd []byte) bool {
-	return bytes.Equal(cmd, BITDEPTH_8_CMD) || bytes.Equal(cmd, BITDEPTH_12_CMD)
-}
-
-func bitdepthCommandToBytesPerSample(cmd []byte) int {
-	if bytes.Equal(cmd, BITDEPTH_8_CMD) {
-		return BYTES_PER_SAMPLE_8BIT
-	} else {
-		return BYTES_PER_SAMPLE_12BIT
-	}
-}
-
 func (backend *DeviceBackend) openSerial(serialName string) (serial.Port, error) {
 	mode := &serial.Mode{
 		BaudRate: 115200,
@@ -414,189 +391,4 @@ func (backend *DeviceBackend) openSerial(serialName string) (serial.Port, error)
 	}
 
 	return port, nil
-}
-
-// Read from the serial port and pipe its signal into the callback, summarizing
-// package units into a buffer. Forward commands from client.
-func connectSerial(ctx context.Context, cancel context.CancelFunc, logger *logrus.Entry, port serial.Port, tx chan interface{}, onReceive func([]byte)) {
-	readerCtx, readerCtxCancel := context.WithCancel(ctx)
-
-	defer func() {
-		logger.Info("Disconnecting from serial port.")
-		port.Close()
-		readerCtxCancel()
-		cancel()
-	}()
-
-	port.ResetInputBuffer() // flush any unread data buffered by the OS
-
-	// For backwards compatibility, we set 8bit depth by default
-	_, err := port.Write(BITDEPTH_8_CMD)
-	if err != nil {
-		logger.WithField("error", err).Info("Failed to set bitdepth of 8.")
-		return
-	}
-	configuredBytesPerSample := BYTES_PER_SAMPLE_8BIT
-
-	// Channel to receive ack that reader is done
-	readerDoneChan := make(chan struct{})
-
-	// Start the initial reader goroutine
-	go readFromPort(readerCtx, logger, port, configuredBytesPerSample, onReceive, readerDoneChan)
-
-	// Forward WebSocket commands to device
-	for {
-		select {
-		case <-ctx.Done():
-			return
-
-		case <-readerDoneChan:
-			return
-
-		case i := <-tx:
-			data, _ := i.([]byte)
-
-			if isBitdepthCommand(data) {
-				newBytesPerSample := bitdepthCommandToBytesPerSample(data)
-
-				// If bytes per sample has changed, we need to restart the reader
-				if newBytesPerSample != configuredBytesPerSample {
-					logger.WithFields(logrus.Fields{
-						"old": configuredBytesPerSample,
-						"new": newBytesPerSample,
-					}).Info("Bytes per sample changed, will restart reader")
-
-					logger.Debug("Sending stop to reader and waiting for ack")
-					readerCtxCancel()
-					<-readerDoneChan
-					logger.Debug("Ack received, reader stopped")
-
-					_, err = port.Write(data)
-					if err != nil {
-						logger.WithField("error", err).Info("Failed to write new bitdepth.")
-						return
-					}
-
-					port.ResetInputBuffer() // flush any data that was not yet read
-
-					configuredBytesPerSample = newBytesPerSample
-
-					readerCtx, readerCtxCancel = context.WithCancel(ctx)
-					readerDoneChan = make(chan struct{})
-
-					// Start a new reader goroutine with updated bytesPerSample
-					go readFromPort(readerCtx, logger, port, configuredBytesPerSample, onReceive, readerDoneChan)
-				}
-			} else {
-				// For non-bitdepth commands, just forward them
-				_, err = port.Write(data)
-				if err != nil {
-					logger.WithField("error", err).Info("Failed to write command to serial port.")
-					return
-				}
-				logger.WithField("bytes", data).Debug("Wrote binary command to serial out: " + string(data))
-			}
-		}
-	}
-}
-
-// Infinite loop for requesting and reading serial data.
-// Stops (returns) upon any error or ctx cancel.
-func readFromPort(
-	ctx context.Context,
-	logger *logrus.Entry,
-	port serial.Port,
-	bytesPerSample int,
-	onReceive func([]byte),
-	doneChan chan<- struct{},
-) {
-	defer func() {
-		// Signal that the reader has completed
-		close(doneChan)
-	}()
-
-	reader := bufio.NewReader(port)
-	state := WAITING_FOR_HEADER
-	var samplesLeftInSet int
-	var bytesLeftInSample int
-
-	// Note: for Flex v4 this command seems to cause the firmware to push
-	// data as fast as we can consume it, whereas for Flex v5 it merely
-	// requests a single frame.
-	START_MEASUREMENT_CMD := []byte{'S', '\n'}
-	_, err := port.Write(START_MEASUREMENT_CMD)
-	if err != nil {
-		logger.WithField("error", err).Info("Failed to write start message to serial port.")
-		return
-	}
-
-	// Start signal acquisition
-	var buff []byte
-	for {
-		// Terminate if we were cancelled
-		if ctx.Err() != nil {
-			logger.Debug("Stopping reader: context cancelled")
-			return
-		}
-
-		input, err := reader.ReadByte()
-		if err != nil {
-			logger.WithField("err", err).Error("Error reading from serial port")
-			return
-		}
-
-		var length_msb byte
-
-		// Finite State Machine for parsing byte stream
-		switch {
-		case state == WAITING_FOR_HEADER && input == HEADER_START_MARKER:
-			state = HEADER_START
-		case state == HEADER_START && input == '\n':
-			state = HEADER_READ_LENGTH_MSB
-		case state == HEADER_READ_LENGTH_MSB:
-			// The number of measurements in each set is given as two
-			// consecutive bytes (big-endian).
-			length_msb = input
-			state = HEADER_READ_LENGTH_LSB
-		case state == HEADER_READ_LENGTH_LSB:
-			length_lsb := input
-			samplesLeftInSet = int(binary.BigEndian.Uint16([]byte{length_msb, length_lsb}))
-			state = WAITING_FOR_BODY
-		case state == WAITING_FOR_BODY && input == BODY_START_MARKER:
-			state = BODY_START
-		case state == BODY_START && input == '\n':
-			state = BODY_READ_SAMPLE
-			buff = []byte{}
-			bytesLeftInSample = bytesPerSample
-		case state == BODY_READ_SAMPLE:
-			buff = append(buff, input)
-			bytesLeftInSample = bytesLeftInSample - 1
-
-			if bytesLeftInSample <= 0 {
-				samplesLeftInSet = samplesLeftInSet - 1
-
-				if samplesLeftInSet <= 0 {
-					// Finish and send set
-					onReceive(buff)
-
-					// Get ready for next set and request it
-					state = WAITING_FOR_HEADER
-					// Optional for Flex v4, mandatory for v5
-					_, err = port.Write(START_MEASUREMENT_CMD)
-					if err != nil {
-						logger.WithField("error", err).Info("Failed to write poll message to serial port.")
-						return
-					}
-				} else {
-					// Start next point
-					bytesLeftInSample = bytesPerSample
-				}
-			}
-		case state == UNEXPECTED_BYTE && input == HEADER_START_MARKER:
-			// Recover from error state when a new header is seen
-			state = HEADER_START
-		default:
-			state = UNEXPECTED_BYTE
-		}
-	}
 }
---
+++ src/dividat-driver/flex/sensitronics/main.go
@@ -0,0 +1,144 @@
+package sensitronics
+
+import (
+	"bufio"
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"github.com/sirupsen/logrus"
+	"go.bug.st/serial"
+)
+
+// Serial communication
+
+const (
+	DRIVER_PROTOCOL_VERSION = 0x02
+	HEADER_START_MARKER     = 0xFF
+	HEADER_TYPE_TERMINATOR  = 0xA // newline
+)
+
+type SensitronicsReader struct{}
+
+func (SensitronicsReader) ReadFromSerial(ctx context.Context, cancel context.CancelFunc, logger *logrus.Entry, port serial.Port, tx chan interface{}, onReceive func([]byte)) {
+	port.ResetInputBuffer() // flush any unread data buffered by the OS
+
+	readerCtx, readerCtxCancel := context.WithCancel(ctx)
+
+	defer func() {
+		logger.Info("Disconnecting from serial port.")
+		port.Close()
+		readerCtxCancel()
+		cancel()
+	}()
+
+	// Channel to receive ack that reader is done
+	readerDoneChan := make(chan struct{})
+
+	// Start the initial reader goroutine
+	go readFromPort(readerCtx, logger, port, onReceive, readerDoneChan)
+
+	// Forward WebSocket commands to device
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case <-readerDoneChan:
+			return
+
+		case i := <-tx:
+			data, _ := i.([]byte)
+			_, err := port.Write(data)
+			if err != nil {
+				logger.WithField("error", err).Info("Failed to write command to serial port.")
+				return
+			}
+			logger.WithField("bytes", data).Debug("Wrote binary command to serial out: " + string(data))
+		}
+	}
+}
+
+func readMessage(reader *bufio.Reader) ([]byte, error) {
+	// read header start marker
+	marker, err := reader.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	if marker != HEADER_START_MARKER {
+		return nil, fmt.Errorf("Expected header start marker, got %u", marker)
+	}
+
+	// read type from header, return will include the HEADER_TYPE_TERMINATOR
+	messageType, err := reader.ReadBytes(HEADER_TYPE_TERMINATOR)
+	if err != nil {
+		return nil, err
+	}
+
+	messageLengthBytesLE := make([]byte, 2, 2)
+	_, err = io.ReadFull(reader, messageLengthBytesLE)
+	if err != nil {
+		return nil, err
+	}
+
+	messageLength := uint(binary.LittleEndian.Uint16(messageLengthBytesLE))
+
+	messageBody := make([]byte, messageLength, messageLength)
+	_, err = io.ReadFull(reader, messageBody)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: bump go to 1.22 and replace with slices.Concat or otherwise
+	totalLength := 1 + len(messageType) + len(messageLengthBytesLE) + len(messageBody)
+	fullMessage := make([]byte, 0, totalLength)
+	fullMessage = append(fullMessage, marker)
+	fullMessage = append(fullMessage, messageType...)
+	fullMessage = append(fullMessage, messageLengthBytesLE...)
+	fullMessage = append(fullMessage, messageBody...)
+
+	return fullMessage, nil
+}
+
+// Infinite loop for requesting and reading serial data.
+// Stops (returns) upon any error or ctx cancel.
+func readFromPort(
+	ctx context.Context,
+	logger *logrus.Entry,
+	port serial.Port,
+	onReceive func([]byte),
+	doneChan chan<- struct{},
+) {
+	defer func() {
+		// Signal that the reader has completed
+		close(doneChan)
+	}()
+
+	reader := bufio.NewReader(port)
+
+	START_MEASUREMENT_CMD := []byte{'S', '\n'}
+	_, err := port.Write(START_MEASUREMENT_CMD)
+	if err != nil {
+		logger.WithField("error", err).Info("Failed to write start message to serial port.")
+		return
+	}
+
+	// Start signal acquisition
+	for {
+		// Terminate if we were cancelled
+		if ctx.Err() != nil {
+			logger.Debug("Stopping reader: context cancelled")
+			return
+		}
+
+		message, err := readMessage(reader)
+		if err != nil {
+			logger.WithField("err", err).Error("Error reading from serial port")
+			return
+		}
+
+		onReceive(message)
+	}
+}
