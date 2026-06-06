e6506a50c46597204a7ffa5a6733811ee2624702
Johannes Emerich <johannes@dividat.com>
2026-06-06 15:57:31 +0200

    Unify nixpkgs dependencies to nixos-26.05 channel
    When splitting nixpkgs dependencies into the "main" version for general
    builds (24.11) and the forward version for macOS builds (25.05), the
    motivation was that 24.11 did not work for macOS, but everything else
    should stay in line with the driver's main environment PlayOS, which was
    pinned to 24.11 at the time.
    
    PlayOS has since been upgraded to 25.11 and may be further upgraded to
    26.05 before the next release. Either way, unifying to a single 26.05
    channel nixpkgs version from does not require significant changes to the
    build definitions relative to 24.11. So we should not expect surprises
    even when building in 25.11 in the context of PlayOS.
    
    Moving everything to 26.05 unifies macOS and other cross-build targets
    again, and allows us to bring in modern versions of developer tools.
    
    In particular, we want to replace npm with pnpm v11.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
     and a development shell for Linux and macOS.
   '';
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.11";
-    # only used for crossBuilding on darwin, since 24.11 is broken
-    nixpkgs2505.url = "github:nixos/nixpkgs/25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs2505, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -18,17 +16,13 @@
         crossBuild = import ./nix/crossBuild.nix {
           inherit pkgs;
         };
-
-        crossBuildDarwin = import ./nix/crossBuild.nix {
-          pkgs = import nixpkgs2505 { inherit system; };
-        };
       in
       {
         devShells = {
           crossBuild = {
             inherit (crossBuild) x86_64-linux;
             inherit (crossBuild) x86_64-windows;
-            inherit (crossBuildDarwin) darwin;
+            inherit (crossBuild) darwin;
           };
           default = import ./nix/devShell.nix {
             inherit pkgs;
